8ca3ed3f91018533baf31b967b228e36ac9308dd
Sai Reddy <reddy.saivenkat555@gmail.com>
2018-09-27 19:01:53 +0530

    Add project files.

---
+++ AzureCosmosDB/Models/CosmosDB/Person.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureCosmosDB.Models.CosmosDB
+{
+    public class Person
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "surname")]
+        public string Surname { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+    }
+}
